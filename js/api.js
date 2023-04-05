1d1b2d10dfae82531dda6c95f2c153bdf6f8434b
rodell1983 <ode21001@byui.edu>
2023-04-04 17:21:22 -0700

    test

---
+++ js/api.js
@@ -0,0 +1,32 @@
+import { parseBool } from './search.js';
+// Load the articles from the API
+async function getArticles() {
+  let size = 100;
+  let start = 0;
+  await fetch(
+    `https://api.spaceflightnewsapi.net/v3/articles?_limit=${size}&_start=${start}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let stringData = JSON.stringify(data);
+      localStorage.setItem("articles", stringData);
+      //search.removeAttribute("disabled");
+    })
+    .catch((error) => notifyUser(error));
+}
+// Load the articles by id from the API
+async function getArticle(id) {
+  let article = "";
+  await fetch(`https://api.spaceflightnewsapi.net/v3/articles/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      article = JSON.stringify(data);
+    })
+    .catch((error) => notifyUser(error));
+
+  return article;
+}
+
+//Start Program
+
+getArticles();
